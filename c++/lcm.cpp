63d4cacc0bab8f0013914c34e524d049bef7901f
Zeyu-Li <zeyu.li23@gmail.com>
2020-12-20 06:54:48 -0600

    added same thing but different function call

--- c++/lcm.cpp
+++ c++/lcm.cpp
@@ -3,8 +3,16 @@
 int lcd(int gcd, int a, int b) {
     return (int)((a*b)/gcd);
 }
-
 int lcd(int a, int b) {
+    // function overload
+    return (int)((a*b)/gcd(a,b));
+}
+
+// different name, same thing
+int lcm(int gcd, int a, int b) {
+    return (int)((a*b)/gcd);
+}
+int lcm(int a, int b) {
     // function overload
     return (int)((a*b)/gcd(a,b));
 }
--- python/lcd.py
+++ python/lcd.py
@@ -8,3 +8,11 @@ def lcd(gcd, int1, int2):
 def lcd_no_gcd(int1, int2):
     from gcd import gcd
     return int((int1*int2)/gcd(int1,int2))
+
+# different name, same thing
+def lcm(gcd, int1, int2):
+    return int((int1*int2)/gcd)
+
+def lcm_no_gcd(int1, int2):
+    from gcd import gcd
+    return int((int1*int2)/gcd(int1,int2))
