7cf9d37e77d3fc81fe12015050bdc5d22a0e8213
Zeyu-Li <zeyu.li23@gmail.com>
2020-12-27 08:54:36 -0600

    finished binary tree

--- c++/structs/binary_tree.cpp
+++ c++/structs/binary_tree.cpp
@@ -23,22 +23,35 @@ class Binary_tree {
         ~Binary_tree();
 
         // prints (from root)
+        // pre-order = Root-Left-Right
+        void pre_order();
+        // in order = Left-Root-Right
         void in_order();
+        // post order = Left-Right-Root
+        void post_order();
 
         // general insert of value (int)
         void insert(int value);
+
+        // searches and returns the node
+        node *search(int value);
     private:
         // root node
         node *root;
 
         // print helper funcs (overloads)
+        void pre_order(node *leaf);
         void in_order(node *leaf);
+        void post_order(node *leaf);
 
         // destroyer helper func
         void delete_node(node *some_node);
 
         // overloading insert of adding root to somewhere in the tree
         void insert(int value, node *leaf);
+        
+        // searches helper func
+        node *search(int value, node *leaf);
 };
 
 // constructor and destructor
@@ -104,6 +117,17 @@ void Binary_tree::delete_node(node *some_node) {
 }
 
 // prints
+void Binary_tree::pre_order() {
+	pre_order(root);
+	std::cout << std::endl;
+}
+void Binary_tree::pre_order(node *leaf) {
+	if (leaf != nullptr) {
+        std::cout << leaf->value << " ";
+        pre_order(leaf->lhs);
+        pre_order(leaf->rhs);
+    }
+}
 void Binary_tree::in_order() {
 	in_order(root);
 	std::cout << std::endl;
@@ -116,8 +140,46 @@ void Binary_tree::in_order(node *leaf) {
         in_order(leaf->rhs);
     }
 }
+void Binary_tree::post_order() {
+	post_order(root);
+	std::cout << std::endl;
+}
+void Binary_tree::post_order(node *leaf) {
+	if (leaf != nullptr) {
+        post_order(leaf->lhs);
+        post_order(leaf->rhs);
+        std::cout << leaf->value << " ";
+    }
+}
 
-int main(int argc, char const *argv[])
+// searches
+node *Binary_tree::search(int value) {
+    // search in root
+    return search(value, root);
+}
+node *Binary_tree::search(int value, node *leaf) {
+    // if not found, returns null ptr
+
+    // search in root
+    if (leaf == nullptr) {
+        return nullptr;
+    } else {
+        if (leaf->value == value) {
+            return leaf;
+        } else if (leaf->value < value) {
+            // goto right sub tree
+            return search(value, leaf->rhs);
+        } else {
+            return search(value, leaf->lhs);
+        }
+        
+    }
+    
+    return search(value, root);
+}
+
+
+int main()
 {
     Binary_tree *tree = new Binary_tree();
 
@@ -128,7 +190,13 @@ int main(int argc, char const *argv[])
     tree->insert(47);
     tree->insert(0);
 
+    tree->pre_order();
     tree->in_order();
+    tree->post_order();
+
+    node *find = tree->search(10);
+    // must set overload to public to do the thing below
+    // tree->post_order(find);
 
     delete tree;
 
