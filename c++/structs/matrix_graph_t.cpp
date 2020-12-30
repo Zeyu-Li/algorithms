2cbe3fd6801d27ca287314ec9f58ea78777810ea
Zeyu-Li <zeyu.li23@gmail.com>
2020-12-30 13:39:10 -0600

    finished matrix graph template

---
+++ c++/structs/matrix_graph_t.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <vector>
+
+// help from https://github.com/DorianDepriester/adjacencyMatrix
+
+
+template<class T>
+class Graph_m {
+    public:
+        // contructors, default undirected
+        Graph_m();
+        // contructor with directness boolean 
+        Graph_m(bool);
+        // contructor with size (number of vertices)
+        Graph_m(int);
+        Graph_m(bool, int);
+        // destructor
+        ~Graph_m();
+
+        // print
+        void print();
+        int get_size();
+
+        // resize
+        void resize(int);
+
+        // setter (edge)
+        void add_edge(int, int);
+        // overloads when default weight is not 1
+        void add_edge(int, int, T);
+
+        // main matrix, therefore getter unnecessary
+        std::vector<std::vector <T>> matrix;
+    private:
+        int size;
+        bool directed;
+};
+
+template<class T>
+Graph_m<T>::Graph_m() {
+    size=0;
+    directed = true;
+}
+template<class T>
+Graph_m<T>::Graph_m(bool direc) {
+    size=0;
+    directed = direc;
+}
+template<class T>
+Graph_m<T>::Graph_m(int n) {
+    directed = true;
+    // resize
+    size=0;
+    resize(n);
+    size=n;
+}
+template<class T>
+Graph_m<T>::Graph_m(bool direc, int n) {
+    directed = direc;
+    // resize
+    size=0;
+    resize(n);
+    size=n;
+}
+template<class T>
+Graph_m<T>::~Graph_m() {
+    size=0;
+}
+
+// resize
+template<class T>
+void Graph_m<T>::resize(int new_size) {
+    if (new_size > size) {
+        // enlarge the matrix
+        for (int i = size; i<new_size; i++) {
+            // add zero to each row
+            // zeros for each row
+            std::vector<T> bottom;
+            for (int j = size; j<new_size; j++) {
+                bottom.push_back(0);
+            }
+            matrix.push_back(bottom);
+        }
+    }
+}
+
+// add edge row then column
+template<class T>
+void Graph_m<T>::add_edge(int x, int y) {
+    // sets size as the max of x or y
+    int max = x > y ? x : y;
+    if (max>size) {
+        size = max;
+        // Resize
+        resize(max);
+    }
+    matrix[x][y] = 1;
+    // if undirected
+    if (!directed) matrix[y][x] = 1;
+}
+template<class T>
+void Graph_m<T>::add_edge(int x, int y, T weight) {
+    // sets size as the max of x or y
+    int max = x > y ? x : y;
+    if (max>size) {
+        size = max;
+    }
+    matrix[x][y] = weight;
+    // if undirected
+    if (!directed) matrix[y][x] = weight;
+}
+
+template<class T>
+int Graph_m<T>::get_size(void) {
+    return size;
+}
+
+template<class T>
+void Graph_m<T>::print(void) {
+    int n = size;
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<n;j++) {
+            std::cout << matrix[i][j]<< "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+
+int main() {
+    Graph_m<float> graph(false, 5);
+    // std::cout << graph.get_size();
+    graph.add_edge(3,2);
+    graph.add_edge(2,1, 2.5);
+
+    graph.print();
+    // std::cout << graph.matrix[1][1];
+    return 0;
+}
