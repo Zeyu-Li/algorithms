bd8e090ea04174cea113d89c32caa9938474d71e
Zeyu-Li <zeyu.li23@gmail.com>
2021-01-03 09:58:11 -0600

    added quick square root

---
+++ c++/alg/q_sqrt.cpp
@@ -0,0 +1,42 @@
+// Quick Square Root (~1% accurancy)
+// from https://www.youtube.com/watch?v=p8u_k2LIZyo&ab_channel=Nemean
+#include <iostream>
+
+
+float q_sqrt(float num) {
+    long address;
+    float x, y;
+    const float factor = 1.5;
+
+    x = num * 0.5;
+    y = num;
+    // IEEE 754 to be stored into long via long pointer cast
+    address = * (long*) &y;
+    address = 0x5f3759df - (address >> 1);
+
+    // cast back
+    y = *(float *) &address;
+    y *= (factor - (x * y *y)); // Newtonian's method to get closer approximation
+    // y *= (factor - (x * y *y)); // Second Iteration of Newtonian's method gets a closer approximation
+
+    return y;
+}
+
+float q_sqrt2(float num) {
+    long address;
+    float x, y;
+    const float factor = 1.5;
+
+    x = num * 0.5;
+    y = num;
+    // IEEE 754 to be stored into long via long pointer cast
+    address = * (long*) &y;
+    address = 0x5f3759df - (address >> 1);
+
+    // cast back
+    y = *(float *) &address;
+    y *= (factor - (x * y *y)); // Newtonian's method to get closer approximation
+    y *= (factor - (x * y *y)); // Second Iteration of Newtonian's method gets a closer approximation
+
+    return y;
+}
