d4cc62b22eef88a753d63b6cc54229779d825c3d
Zeyu-Li <zeyu.li23@gmail.com>
2021-01-07 14:44:40 -0600

    finished dijkstra's alg

---
+++ c++/alg/dijkstra.cpp
@@ -0,0 +1,75 @@
+/* Dijkstra's alg */ 
+#include <vector>
+#include <algorithm>
+#include <limits.h> 
+#include "../structs/matrix_graph.cpp"
+
+
+class Graph_dijkstra: public Graph_m {
+    public:
+        Graph_dijkstra(): Graph_m() {};
+        Graph_dijkstra(bool a): Graph_m(a) {};
+        Graph_dijkstra(int a): Graph_m(a) {};
+        Graph_dijkstra(bool a, int b): Graph_m(a, b) {};
+        // returns a vector of the result of dijkstra's alg
+        std::vector<int> dijkstra(int);
+        // stores dijkstra results in second argument
+        void dijkstra(int, std::vector<int>&);
+        // returns shortest path given start and end
+        int dijkstra(int, int);
+    private:
+        // gets min distance
+        int min_dist(std::vector<int>&, std::vector<bool>&);
+};
+
+int Graph_dijkstra::min_dist(std::vector<int>& dist, std::vector<bool>& shortest_path) {
+    int min = INT_MAX, min_index;
+    // for each vertex
+    for (int v = 0; v < size; v++) {
+        // if is connected and smaller than current min, swap to that one
+        if ((!shortest_path[v]) && dist[v] <= min) {
+            min = dist[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+
+std::vector<int> Graph_dijkstra::dijkstra(int start) {
+    std::vector<int> dist(size); // shortest path distance of each point
+    std::fill_n(dist.begin(), size, INT_MAX);
+    std::vector<bool> shortest_path(size);
+    std::fill_n(shortest_path.begin(), size, false);
+
+    // start of path is of distance 0
+    dist[start] = 0;
+    // for every vertex
+    for (int i = 0; i < size - 1; i++) {
+        // gets shortest path to work off of
+        int u = min_dist(dist, shortest_path);
+        // check the current node
+        shortest_path[u] = true;
+
+        // update dist of adjacent vertices to the shortest path if it is connect and shorter than previous entry (dp)
+        for (int v = 0; v < size; v++) {
+            if (!shortest_path[v] && matrix[u][v] && dist[u] != INT_MAX && dist[u] + matrix[u][v] < dist[v]) {
+                dist[v] = dist[u] + matrix[u][v];
+            }
+        }
+    }
+    // return the distances of all nodes
+    return dist;
+}
+
+void Graph_dijkstra::dijkstra(int start, std::vector<int>&path) {
+    std::vector<int> d_path = dijkstra(start);
+    // copy vectors
+    for(int i=0; i<d_path.size(); ++i) path.push_back(d_path[i]);
+}
+
+int Graph_dijkstra::dijkstra(int start, int end) {
+    std::vector<int> d_path = dijkstra(start);
+    // return length of shortest path
+    return d_path[end];
+}
--- c++/tests/test_alg.cpp
+++ c++/tests/test_alg.cpp
@@ -6,6 +6,7 @@
 #include "../alg/lcm.cpp"
 #include "../alg/q_sqrt.cpp"
 #include "../alg/bfs.cpp"
+#include "../alg/dijkstra.cpp"
 using namespace std;
 
 void print(int size, int arr[]) {
@@ -27,10 +28,8 @@ int main() {
     float num = 10;
     cout << "Quick Square Root:\n" << q_sqrt(num) << " : " << q_sqrt2(num) << endl;
 
-    // bfs
-    
-    /* list graph */
-    cout << "List graph:" << endl;
+    /* BFS */
+    cout << "BFS:" << endl;
     int size = 8;
     Graph_bfs graph_l(true, size);
     graph_l.add_edge(0, 1);
@@ -43,14 +42,44 @@ int main() {
     graph_l.add_edge(5, 6);
     graph_l.add_edge(6, 7);
     int source = 0, dest = 7;
-    vector<int> path;
-    graph_l.print();
-    graph_l.bfs(source);
-    cout << "Length: " << graph_l.bfs_path(source, dest, path) << endl;
+    vector<int> path_bfs1;
+    vector<int> path_bfs2;
+    // graph_l.print();
+    graph_l.bfs(source, path_bfs1);
+    cout << "Length: " << graph_l.bfs_path(source, dest, path_bfs2) << endl;
 
     // print
+    cout << "BFS order: ";
+    for(int i=0; i<path_bfs1.size(); ++i) cout << path_bfs1[i] << ' ';
+    cout << endl;
     cout << "Shortest path: ";
-    for(int i=0; i<path.size(); ++i) cout << path[i] << ' ';
+    for(int i=0; i<path_bfs2.size(); ++i) cout << path_bfs2[i] << ' ';
+    cout << endl;
+
+    /* Dijkstra */
+    cout << "Dijkstra:" << endl;
+    Graph_dijkstra Dijkstra(false, 9);
+    Dijkstra.add_edge(0, 1, 4);
+    Dijkstra.add_edge(0, 7, 8);
+    Dijkstra.add_edge(1, 2, 8);
+    Dijkstra.add_edge(1, 7, 11);
+    Dijkstra.add_edge(2, 3, 7);
+    Dijkstra.add_edge(2, 5, 4);
+    Dijkstra.add_edge(2, 8, 2);
+    Dijkstra.add_edge(3, 4, 9);
+    Dijkstra.add_edge(3, 5, 14);
+    Dijkstra.add_edge(4, 5, 10);
+    Dijkstra.add_edge(5, 6, 2);
+    Dijkstra.add_edge(6, 7, 1);
+    Dijkstra.add_edge(6, 8, 6);
+    Dijkstra.add_edge(7, 8, 7);
+
+    // Dijkstra.print();
+    vector<int> d_path;
+    Dijkstra.dijkstra(0, d_path);
+    for(int i=0; i<d_path.size(); ++i) cout << i << ": " << d_path[i] << ' ' << endl;
+    cout << endl;
+
 
     return 0;
 }
