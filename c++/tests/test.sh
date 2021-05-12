1f27156df9b5f514a9728a245ee08cf61777728a
Zeyu-Li <zeyu.li23@gmail.com>
2021-05-12 17:19:38 -0600

    updated tests

--- c++/tests/test.sh
+++ c++/tests/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Testing: "
-g++ test_structs.cpp -o test_structs && ./test_structs > test1.out
-g++ test_structs_template.cpp -o test_structs_template && ./test_structs_template > test2.out
-g++ test_alg.cpp -o test_alg && ./test_alg > test3.out
+gcc test_structs.cpp -o test_structs && ./test_structs > test1.out
+gcc test_structs_template.cpp -o test_structs_template && ./test_structs_template > test2.out
+gcc test_alg.cpp -o test_alg && ./test_alg > test3.out
 
 diff test1.out test_structs.out
 diff test2.out test_structs_template.out
