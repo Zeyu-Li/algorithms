d44ad914fc0602755e045ecb0a12aa5a0283830a
Zeyu-Li <zeyu.li23@gmail.com>
2021-05-12 18:02:10 -0600

    updated test script

--- c++/tests/test.sh
+++ c++/tests/test.sh
@@ -4,7 +4,12 @@ g++ test_structs.cpp -o test_structs && ./test_structs > test1.out
 g++ test_structs_template.cpp -o test_structs_template && ./test_structs_template > test2.out
 g++ test_alg.cpp -o test_alg && ./test_alg > test3.out
 
-diff test1.out test_structs.out
-diff test2.out test_structs_template.out
-diff test3.out test_alg.out
-rm -f test1.out test2.out test3.out
+diff test1.out test_structs.out > diff.out
+diff test2.out test_structs_template.out >> diff.out
+diff test3.out test_alg.out >> diff.out
+
+if [ -s diff.out ] # test if diff is empty
+then
+    exit 1 # test failed
+fi
+
